b92f8ab87ef1bdf9d43b9ec3e58a528b919e5ccb
Anshita Bajpai <anshitapsit@gmail.com>
2026-02-03 21:27:28 +0530

    refactor

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { CartContext } from "../context/CartContext.jsx";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
