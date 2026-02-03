e65cd80eb2977644039d3dd6a670bec004e9c7ef
Anshita Bajpai <anshitapsit@gmail.com>
2026-02-03 21:19:48 +0530

    email validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"],
+  },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], default: "user" },
 });
