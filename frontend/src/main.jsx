1233ab69d38b3f587260cd59174d410982a43b71
Anshita Bajpai <anshitapsit@gmail.com>
2026-02-20 16:40:56 +0530

    wishlist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import App from "./App";
 import { CartProvider } from "./context/CartContext.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-
+import { WishlistProvider } from "./context/WishlistContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <Router>
-        <App />
-      </Router>
-    </CartProvider>
-  </React.StrictMode>
+    <Router>
+      <CartProvider>
+        <WishlistProvider>
+          <App />
+        </WishlistProvider>
+      </CartProvider>
+    </Router>
+  </React.StrictMode>,
 );
